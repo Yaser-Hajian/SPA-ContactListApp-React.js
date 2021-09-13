d0a815bf35b0b877f09d467cda15bd269f8064ba
Yaser <mohammadyaser1379@gmail.com>
2021-09-13 19:05:29 +0430

    add axios to project and each contact add to DB and delete from DB

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
 import './App.css';
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import AddContact from "./Components/AddContact/AddContact";
 import ContactsList from "./Components/ContactsList/ContactsList";
+import axios from "axios";
 
 function App() {
     const [contactsList, setContactsList] = useState([]);
+    useEffect(()=>{
+        axios.get("http://localhost:3001/contacts")
+            .then(response => {
+                const newContactsList = [];
+                if (response){
+                    response.data.map(contact => {newContactsList.unshift(contact)});
+                    setContactsList(newContactsList);
+                }
+            })
+            .catch(error => console.error(error));
+    },[]);
     const addContactHandler = (contact) => {
         const newContactsList = [...contactsList];
         const date = new Date();
         const newContact = {id:date.getTime() , ...contact};
         newContactsList.unshift(newContact);
-        setContactsList(newContactsList);
+        setContactsList(newContactsList)
+        axios.post("http://localhost:3001/contacts" , newContact)
+            .then()
+            .catch(error => console.log(error));
+
     }
     const deleteContactHandler = (id) => {
         const newContactList = contactsList.filter(contact => contact.id !== id);
         setContactsList(newContactList);
+        axios.delete("http://localhost:3001/contacts/"+id)
+            .then()
+            .catch(error => console.log(error));
     }
     return (
         <div className="App">
