bcee1156603f6d894dcb40ee33565f194449e4c9
Yaser <mohammadyaser1379@gmail.com>
2021-09-13 11:57:42 +0430

    add user icon for Contact

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import style from './ContatcStyle.module.css'
+import user from '../../Assets/Images/user.png'
 const Contact = ({name, email, onDelete}) => {
     return (
         <div className={style.container}>
+            <img src={user} alt="user-icon"/>
             <p>name: {name}</p>
             <p>email: {email}</p>
             <div className={style.options}>
