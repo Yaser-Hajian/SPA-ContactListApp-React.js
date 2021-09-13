2846ea3f84424a4eb28f990fa9977eac1348d84f
Yaser <mohammadyaser1379@gmail.com>
2021-09-13 10:29:27 +0430

    create Contact component

---
+++ src/Components/Contact/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = ({name, email}) => {
+    return (
+        <div>
+            <p>name: {name}</p>
+            <p>email: {email}</p>
+        </div>
+    );
+};
+
+export default Contact;
